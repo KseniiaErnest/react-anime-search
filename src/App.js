2623d7cecbc8f41f728bb995f165420b6c35a38e
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-12-11 10:17:40 -0500

    useLocalStorage fix

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Error from "./components/Error";
 import AnimeDetails from "./components/AnimeDetails";
 import WatchedAnimeSummary from "./components/WatchedAnimeSummary";
 import WatchedAnimeList from "./components/WatchedAnimeList";
+import useLocalStorage from "./components/useLocalStorage";
 
 function App() {
   const [anime, setAnime] = useState([]);
@@ -16,11 +17,13 @@ function App() {
   const [error, setError] = useState('');
   const [query, setQuery] = useState('');
   const [selectId, setSelectId] = useState(null);
+
+  const [watchedAnime, setWatchedAnime] = useLocalStorage([], 'watchedAnime');
   // const [watchedAnime, setWatchedAnime] = useState([]);
-  const [watchedAnime, setWatchedAnime] = useState(function() {
-    const storedValue = localStorage.getItem('watchedAnime');
-    return JSON.parse(storedValue);
-  });
+  // const [watchedAnime, setWatchedAnime] = useState(function() {
+  //   const storedValue = localStorage.getItem('watchedAnime');
+  //   return JSON.parse(storedValue);
+  // });
   // const tempQueary = 'naruto';
 
   function handleSelectAnime(id) {
@@ -41,9 +44,9 @@ function App() {
     setWatchedAnime((currentWatched) => currentWatched.filter((animeToDelete) => animeToDelete.animeId !== id));
   };
 
-  useEffect(function() {
-    localStorage.setItem('watchedAnime', JSON.stringify(watchedAnime))
-  }, [watchedAnime])
+  // useEffect(function() {
+  //   localStorage.setItem('watchedAnime', JSON.stringify(watchedAnime))
+  // }, [watchedAnime])
   
 
   useEffect(function () {
